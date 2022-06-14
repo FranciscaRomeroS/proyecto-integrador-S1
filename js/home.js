b7264cc56bb552a010ce6fd430cf8a17dd4e7aab
pilimedico <pmedico@udesa.edu.ar>
2022-06-14 16:55:10 -0300

    home fetch album y canciones

--- js/home.js
+++ js/home.js
@@ -11,17 +11,22 @@ fetch(url)
     .then(function (data) {
         console.log(data);
         let tracks = data.artists.data;
+        let albums = data.albums.data;
+        let canciones = data.tracks.data;
+        
 
 
         //para capturar el elemento (que lo llamo en contendor)
 
         let sectionArtistas = document.querySelector('.cajaPadre2');
+        let sectionAlbums = document.querySelector('.cajaPadre3');
+        let sectionCanciones = document.querySelector('.cajaPadre4');
 
         //adentro del contenedor quiero agregarle la infromacion, como es un array, lo hago a tarves del for
 
-        for (let i = 0; i < tracks.length; i++) {
+        for (let i = 0; i < 5; i++) {
             sectionArtistas.innerHTML += `<article class="Artistas">
-                                        <a href="./artist.html">
+                                        <a href="./artist.html?id=${tracks[i].id}"> 
                                             <img class="top2" src="${tracks[i].picture_medium}" alt="">            
                                         </a>
                                         <a class="letra" href="./artist.html">
@@ -30,8 +35,34 @@ fetch(url)
                                     </article>`;
             
         }
+
+        for (let i = 0; i <5; i++) {
+            sectionAlbums.innerHTML += `<article class="Albumes">
+            <a href="./album.html">
+            <img class="top2" src="${albums[i].cover_medium}" alt="">  
+            </a>
+            <a class="letra" href="./album.html">
+                <h4>${albums[i].title}</h4>
+            </a>
+        </article>`;
+        } 
+
+        for (let i = 0; i<5; i++){
+            sectionCanciones.innerHTML += `<article class="Canciones">
+            <a href="./canciones.html">
+                  <img class="top2" src="${canciones[i].album.cover_medium}" alt="">
+            </a>
+            <a class="letra" href="./canciones.html">
+                <h4>${canciones[i].title}</h4>
+            </a>
+           
+   
+        </article>`
+        }
+
     })
 
     .catch (function (error) {
-console.log("Error: " + error);
+ console.log("Error: " + error);
     })
+
