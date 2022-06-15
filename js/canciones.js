1477c5c2820a2ba50309f0a4cbd1b58fb055e934
FranciscaRomeroS <fromerosaha@udesa.edu.ar>
2022-06-15 11:36:59 -0300

    canciones

--- js/canciones.js
+++ js/canciones.js
@@ -1,11 +1,33 @@
-let proxi = `https://api.allorigins.win/raw?url=endpointAPI`;
-let endpoint = `https://api.deezer.com/track/3135556`;
-let url = proxi+endpoint;
+let qs = location.search;
+let qsol = new URLSearchParams(qs);
+let id = qsol.get('id')
+
+const url = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`
+let idAGuardar=id
 
 fetch(url)
     .then(function(response){
         return response.json();
-    }).then(function(data){
-        console.log(data);
     })
+    .then(function(data){
+        console.log(data);
+
+        let titulo = document.querySelector('.letra')
+        let foto = document.querySelector('.top2')
+
+        titulo.innerText = data.title;
+    
+
+        let infoCanciones = document.querySelector('.letra');
+        
+
+
+    })
+    .catch(function(error) {
+        console.log( 'El error fue '+ error);
+    })
+
+ 
+
+
         
