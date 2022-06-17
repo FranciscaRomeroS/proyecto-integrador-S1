2db2eeb716f84c9628a908f2ce9db8c7f8f42546
pilimedico <pmedico@udesa.edu.ar>
2022-06-16 22:19:20 -0300

    canciones y playlist funciona

--- js/canciones.js
+++ js/canciones.js
@@ -2,6 +2,7 @@ let qs = location.search;
 let qsol = new URLSearchParams(qs);
 let id = qsol.get('id');
 let titulo = document.querySelector('.tema')
+let sectionAlbumes = document.querySelector('.cajaPadre3')
         let portada = document.querySelector('.top2')
         let cantante = document.querySelector('.link1')
         let favorito = document.querySelector('.submit')
@@ -20,6 +21,25 @@ fetch(url)
         cantante.innerText = data.artist.name;
        
         idGuardar=data.id
+
+        for (let i = 0; i < 5; i++)
+        {
+            sectionAlbumes.innerHTML += `<article class="Albumes">
+            <a href="./album.html">
+              <img class="top2" src="./css/img/miley-rosa.jpg" alt="">  
+            </a>
+            <h3 class="letra">Angels Like You</h3>
+            <a class="link1" href="./artist.html">Miley Cyrus</a>
+            <br>
+            
+            <br>
+            <form action="./playlist.html" method="GET">
+                <button class="submit" type="submit">Agregar a mi Playlist</button>
+            </form>
+            <br>
+            <a class="linkplaylist" href="./playlist.html">Ir a mi Playlist</a>
+        </article>`
+        }
     })
     .catch(function(error){
         console.log('El error es' + error);
